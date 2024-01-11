52cc4369931f62e7cdd269c36e7aee4fa4cf0b70
Larry Welch <larry.welch@foundationtek.com>
2024-01-11 13:03:11 -0700

    libraries with internal and external examples

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ A python library containing the file processing functions for AgriStat CSV and P
 
 The goal of the library is to create reusable code so that new partner files can be added quickly.  It is understood that each partner will have specific requirements, but each will likely be CSV or PDF based.
 
+# Azure Library
+The dsmAzureLib provides features for the interacting with Azure - currently limited to Azure Storage
+
 # CSV Library
-The dsmCSVLib provides features for uploading csv files to azure, parsing csv files to then be processed into a sql database.
+The dsmCSVLib provides features for parsing csv files to then be processed into a sql database.
 
 # PDF Library
 The dsmPDFLib provides features for uploading pdf files to azure, parsing csv files to then be processed into a sql database.
 
 # Environment Variables
 The libraries, examples, and tests utilize environment variables.  The .gitignore has been updated to exclued set-env-vars.bat.  It's best to create this file within the library folders for setting the values.
+
 
--- examples/pdfLib/main.py
+++ examples/pdfLib/main.py
@@ -1,15 +1,65 @@
-from pdfLib import myFunctions
+#
+#
+#
 
-print('Simple Use of PDF Library')
+from io import BufferedReader
+import os
+from dsmPdfLib.pdfUtil import pdfUtil
 
-def test_haversine():
-  print('executing test...')
+def getUtil() -> pdfUtil:
+  print('[getUtil] entered...')
   
-    # Amsterdam to Berlin
-  assert myFunctions.haversine(
-        4.895168, 52.370216, 13.404954, 52.520008
-    ) == 576.6625818456291
-    
-test_haversine()
+  # get the pdf env vars
+  pdfSvcClientId = os.getenv('PDF_SERVICES_CLIENT_ID')  
+  pdfSvcClientSecret = os.getenv('PDF_SERVICES_CLIENT_SECRET')
+  assert pdfSvcClientId and pdfSvcClientSecret
+  util = pdfUtil(pdfSvcClientId, pdfSvcClientSecret)
+  
+  print('[getUtil] success!')
+  return util
+ 
+def getBufferedReader(util: pdfUtil) -> BufferedReader:
+  print('[getBufferedReader] Starting...')
+  
+  # Get the sample file - note that this tests expects a folder to exist (sample-pdfs) and for it to contain a sample.pdf file
+  base_path = os.path.dirname(os.path.realpath(__file__))
+  samplesDir =os.path.abspath(os.path.join(base_path,'sample-pdfs'))
+  fileName = os.path.join(samplesDir, 'sample.pdf')
+  print('[getBufferedReader] processing file: ', fileName)
+  
+  reader = util.getBufferedReader(fileName)
+  assert reader != None
+  
+  print('[getBufferedReader] success!')
+  return reader
+  
+def extract(reader: BufferedReader, util:pdfUtil):
+  print('[extract] entered...')
+  
+  # Use the pdf utility to extract the file.  The results can be saved to a zip file
+  print('[extract] call the dsmPdfUtil.extractFile()...')
+  fileRef = util.extractFile(reader)
 
-print('Complete.')
+  # Save the extract results to a zip file
+  zipFile = 'sample_pdf.zip'  
+  if os.path.isfile(zipFile):
+    os.remove(zipFile)    
+  fileRef.save_as(zipFile)
+  print('[extract] success! Results saved to:', zipFile)
+
+def main() :
+  print('Executing getUtil()...')
+  util = getUtil()
+  print('')
+  
+  print('executing getBufferedReader()...')
+  reader = getBufferedReader(util)
+  print('')
+  
+  print('Executing extract()...')
+  extract(reader, util)
+  print('')
+  
+  print('Complete.')
+  
+main()
